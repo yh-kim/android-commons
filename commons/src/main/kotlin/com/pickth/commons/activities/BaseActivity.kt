07da466400454b00e65acacd9b92e4950ca23745
yh-kim <btc_yh@naver.com>
2017-07-18 12:51:37 +0900

    Add extensions, base activity

---
+++ commons/src/main/kotlin/com/pickth/commons/activities/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.pickth.commons.activities
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by yonghoon on 2017-07-18.
+ * Blog   : http://blog.pickth.com
+ * Github : https://github.com/yh-kim
+ * Mail   : yonghoon.kim@pickth.com
+ */
+class BaseActivity: AppCompatActivity() {
+}
