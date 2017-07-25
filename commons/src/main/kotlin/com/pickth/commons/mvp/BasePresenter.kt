caaf846341eaf167cf62901bf1313a5abdf690eb
Yonghoon Kim <btc_yh@naver.com>
2017-07-25 19:28:22 +0900

    Change parameter of base presenter

--- commons/src/main/kotlin/com/pickth/commons/mvp/BasePresenter.kt
+++ commons/src/main/kotlin/com/pickth/commons/mvp/BasePresenter.kt
@@ -16,9 +16,6 @@
 
 package com.pickth.commons.mvp
 
-import android.content.Context
-import android.view.View
-
 interface BasePresenter {
-    fun attachView(view: View, context: Context)
+    fun attachView(view: BaseView<*>)
 }
